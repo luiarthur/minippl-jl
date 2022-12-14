070ecaf744e746a5a10576a1ca2e471b8f0dca30
Arthur Lui <luiarthur@gmail.com>
2022-12-14 10:01:10 -0700

    refactored

--- scripts/demo.jl
+++ scripts/demo.jl
@@ -11,7 +11,7 @@ function PPL.model(m::PPL.Model{NormalModel}; x)
     sigma = PPL.rv(m, :sigma, Uniform(0, 1))
     PPL.rv(m, :x, Normal(mu, sigma), obs=x)
 end
-nm = PPL.Model{NormalModel}()
+m = PPL.Model{NormalModel}()
 # Want to abbreviate the above to:
 # @model function NormalModel(x)
 #     mu = PPL.rv(:mu, Normal())
@@ -23,10 +23,10 @@ seed!(0)
 true_dist = Normal(3, 0.5)
 x = rand(true_dist, 100)
 
-t1 = get(trace(condition(nm, Dict(:mu => 0))), x = 10)
-t2 = get(trace(condition(nm, Dict(:sigma => 0.01))),
+t1 = get(trace(condition(m, Dict(:mu => 0))), x = 10)
+t2 = get(trace(condition(m, Dict(:sigma => 0.01))),
          x = nothing)
-t3 = get(trace(condition(nm, Dict(:mu => 11, :sigma => 0.01))),
+t3 = get(trace(condition(m, Dict(:mu => 11, :sigma => 0.01))),
          x = 10)
 
 for (i, t) in enumerate([t1, t2,t3])
@@ -39,14 +39,14 @@ end
 
 xs = rand(true_dist, 2000)
 profile = OrderedDict(
-    m => PPL.logpdf(
-        nm,
-        Dict(:mu => m, :sigma => 0.5),
+    mu => PPL.logpdf(
+        m,
+        Dict(:mu => mu, :sigma => 0.5),
         x = xs
     )
-    for m in range(2, 4, 15)
+    for mu in range(2, 4, 15)
 )
 
-for (m, lpdf) in profile
-    @printf "m: %.3f | lpdf: %.3f\n" m lpdf
+for (mu, lpdf) in profile
+    @printf "mu: %.3f | lpdf: %.3f\n" mu lpdf
 end
